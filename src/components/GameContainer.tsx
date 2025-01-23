702aefa9f27b0e29de16edd3f149f8a0f8644e96
Ronit Gurung <lecrepes45@gmail.com>
2025-01-23 18:05:46 +0000

    loading animation

--- src/components/GameContainer.tsx
+++ src/components/GameContainer.tsx
@@ -1,4 +1,5 @@
 import { FaHeart } from 'react-icons/fa';
+import { useState, useEffect } from 'react';
 
 interface GameContainerProps {
   gameData: {
@@ -16,6 +17,28 @@ interface GameContainerProps {
 }
 
 export default function GameContainer({ gameData }: GameContainerProps) {
+  const [bootAnimation, setBootAnimation] = useState<boolean>(false);
+  const [loadingText, setLoadingText] = useState<string>('');
+  const bootText = 'loading...';
+
+  useEffect(() => {
+    if (start) {
+      setBootAnimation(true);
+      let currentIndex = 0;
+      const interval = setInterval(() => {
+        if (currentIndex < bootText.length) {
+          setLoadingText(bootText.slice(0, currentIndex + 1));
+          currentIndex++;
+        } else {
+          setTimeout(() => {
+            setBootAnimation(false);
+          }, 1000);
+          clearInterval(interval);
+        }
+      }, 100);
+      return () => clearInterval(interval);
+    }
+  }, []);
   const {
     start,
     highScore,
@@ -34,34 +57,40 @@ export default function GameContainer({ gameData }: GameContainerProps) {
         isAnimating ? 'turnOn' : 'turnOff'
       }  ${start ? 'visible' : 'invisible'}`}
     >
-      <div className="flex flex-row justify-between text-center ">
-        <div className="text-4xl w-1/3" data-testid="score">
-          score: {score} highScore: {highScore}
-        </div>
-        <div className="text-8xl  w-1/3" data-testid="time">
-          {time}
-        </div>
-        <div className="text-4xl w-1/3 flex flex-row gap-5 justify-center">
-          {Array.from({ length: lives }, (_, index) => (
-            <FaHeart key={index} data-testid="heart-icon" />
-          ))}
-        </div>
-      </div>
-      <div className=" h-1/2d">
-        <div className="text-6xl" data-testid="category">
-          {category}
-        </div>
-        {process.env.NODE_ENV === 'development' ? <div>{word}</div> : null}
-
-        <div className="flex flex-row gap-5 justify-center ">
-          {solved.map((letter, index) => (
-            <div className="text-8xl" key={index} data-testid="letter">
-              {letter}
+      {bootAnimation ? (
+        <div>{loadingText}</div>
+      ) : (
+        <>
+          <div className="flex flex-row justify-between text-center ">
+            <div className="text-4xl w-1/3" data-testid="score">
+              score: {score} highScore: {highScore}
             </div>
-          ))}
-        </div>
-      </div>
-      <div className="text-8xl">{key} </div>
+            <div className="text-8xl  w-1/3" data-testid="time">
+              {time}
+            </div>
+            <div className="text-4xl w-1/3 flex flex-row gap-5 justify-center">
+              {Array.from({ length: lives }, (_, index) => (
+                <FaHeart key={index} data-testid="heart-icon" />
+              ))}
+            </div>
+          </div>
+          <div className=" h-1/2d">
+            <div className="text-6xl" data-testid="category">
+              {category}
+            </div>
+            {process.env.NODE_ENV === 'development' ? <div>{word}</div> : null}
+
+            <div className="flex flex-row gap-5 justify-center ">
+              {solved.map((letter, index) => (
+                <div className="text-8xl" key={index} data-testid="letter">
+                  {letter}
+                </div>
+              ))}
+            </div>
+            <div className="text-8xl h-1/8 w-1/8 bg-blue-50">{key} </div>
+          </div>
+        </>
+      )}
     </div>
   );
 }
